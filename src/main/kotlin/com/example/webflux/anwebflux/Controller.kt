0c96af39efebfe79bd2fa27826e7af524e66ab02
digimon <digimon.1740@gmail.com>
2021-03-07 17:59:50 +0900

    수정

--- src/main/kotlin/com/example/webflux/anwebflux/Controller.kt
+++ src/main/kotlin/com/example/webflux/anwebflux/Controller.kt
@@ -4,25 +4,33 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toFlux
+import reactor.kotlin.core.publisher.toMono
 
 @RestController
 @RequestMapping("/users")
-class Controller(
-    val service: Service
-) {
+class Controller {
 
     @GetMapping
-    fun getAll(): Flux<User> = service.getAll()
+    fun getAll(): Flux<User> = users.toFlux()
 
     @GetMapping("/{id}")
     fun get(@PathVariable("id") id: String): Mono<User> =
-        service.get(id)
+        users.findLast { id == it.id }.toMono()
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun save(@RequestBody user: User): Mono<Void> {
-        service.add(user)
+        users.add(user)
         return Mono.empty()
     }
-}
 
+
+    val users = mutableListOf(
+        User(id = "user1@gmail.com"),
+        User(id = "user2@gmail.com")
+    )
+
+
+    class User(val id: String)
+}
--- src/main/kotlin/com/example/webflux/anwebflux/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.webflux.anwebflux
-
-class User(val id: String)
-
-val users = mutableSetOf(
-    User(id = "user1@gmail.com"),
-    User(id = "user2@gmail.com")
-)
